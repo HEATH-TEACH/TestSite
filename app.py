e3f1fa2e3c0aff7846bc58c66e6ab9fd77c1cefd
DHEATH <dheath@mbhs.sa.edu.au>
2026-05-13 03:59:30 +0000

    extra pages added

--- app.py
+++ app.py
@@ -7,9 +7,37 @@ app = Flask(__name__)
 def home_page():
     return render_template('index.html')
 
-@app.route('/page2')
-def page2():
-    return render_template('page2.html')
+@app.route('/basic')
+def basic():
+    return render_template('basic.html')
+
+@app.route('/pro')
+def pro():
+    return render_template('pro.html')
+   
+@app.route('/rare')
+def rare():
+    return render_template('rare.html')
+
+@app.route('/care')
+def care():
+    return render_template('care.html')
+
+@app.route('/about_us')
+def about_us():
+    return render_template('about_us.html')
+
+@app.route('/contact_us')
+def contact_us():
+    return render_template('contact_us.html')
+
+@app.route('/terms_and_conditions')
+def terms_and_conditions():
+    return render_template('terms_and_conditions.html')
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000)
